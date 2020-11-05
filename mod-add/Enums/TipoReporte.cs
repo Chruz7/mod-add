1901b8d7c29452efe4de8c9b87c78f2817804501
Chruz7 <albertojavier_cruzchable@outlook.com>
2020-11-04 21:24:59 -0600

    inicio del formulario para el corte z

---
+++ mod-add/Enums/TipoReporte.cs
@@ -0,0 +1,10 @@
+﻿namespace mod_add.Enums
+{
+    public enum TipoReporte
+    {
+        RESUMIDO,
+        DETALLADO_VERTICAL,
+        DETALLADO_HORIZONTAL,
+        DETALLADO_FORMAS_PAGO
+    }
+}
---
+++ mod-add/Selectores/Reporte.cs
@@ -0,0 +1,10 @@
+﻿using mod_add.Enums;
+
+namespace mod_add.Selectores
+{
+    public class Reporte
+    {
+        public string DisplayText { get; set; }
+        public TipoReporte TipoReporte { get; set; }
+    }
+}
